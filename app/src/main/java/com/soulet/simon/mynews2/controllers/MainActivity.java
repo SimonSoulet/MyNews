aaa5e8535c07c64c7774471398d5c6dfaddafd5d
Simon <simon.soulet@gmail.com>
2018-09-03 14:27:54 +0200

    Project initialisation + package + folder screen size

---
+++ app/src/main/java/com/soulet/simon/mynews2/controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soulet.simon.mynews2.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.soulet.simon.mynews2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
