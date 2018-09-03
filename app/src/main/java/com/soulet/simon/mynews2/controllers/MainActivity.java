a51560671518199ffa52e51ed6c768abb917a376
Simon <simon.soulet@gmail.com>
2018-09-03 16:35:34 +0200

    Implementation toolbar

--- app/src/main/java/com/soulet/simon/mynews2/controllers/MainActivity.java
+++ app/src/main/java/com/soulet/simon/mynews2/controllers/MainActivity.java
@@ -1,7 +1,11 @@
 package com.soulet.simon.mynews2.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.soulet.simon.mynews2.R;
 
@@ -11,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activty_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_params:
+
+                return true;
+            case R.id.menu_activity_main_search:
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+                this.startActivity(searchIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void configureToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
